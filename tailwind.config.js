92e9f515e93e9c3d76e77ca2ab3b19c3de3ab9ac
Cod3Andy <derangedjk75@gmail.com>
2023-08-04 00:35:49 +0300

    LoginRequest+RedirectCallbackEndpoint

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        'metropolis': ['Metropolis', 'sans-serif'],
+      },
+      colors: {
+        'bg-color': 'var(--bg-color)',
+        'sidebar-color': 'var(--sidebar-color)',
+        'header-color': 'var(--header-color)',
+        'text-color': 'var(--text-color)',
+        'accent-color': 'var(--accent-color)',
+        'menu-background-color': 'var(--menu-background-color)',
+        'light-gray': 'var(--light-gray)',
+        'medium-gray': 'var(--medium-gray)',
+        'dark-gray': 'var(--dark-gray)',
+        'menu-text-color': 'var(--menu-text-color)',
+        'border': 'var(--border)',
+        // 'sidebar-width': 'var(--sidebar-width)',
+        // 'header-height': 'var(--header-height)',
+        'error': 'var(--error)',
+        'warning':'var(--warning)',
+      },
+    },
+  },
+  plugins: [],
+}
+
