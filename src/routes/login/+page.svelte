daf1bb233864b0a4622b5c5c47aa3a1147a94fd4
Cod3Andy <derangedjk75@gmail.com>
2023-08-06 00:33:02 +0300

    fix: page styling

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -4,12 +4,12 @@
     import cover from '$assets/cover.png';
 </script>
 
-<div class="wrapper">
-    <div class="content">
+<div class="wrapper flex flex-col md:flex-row items-center w-full max-w-[1200px] mt-[80px] mx-auto mb-0">
+    <div class="content flex-[1.8]">
         <img src={logo} class="logo" alt="Spotify" />
         <h1 class="">EGP 0.00 for 3 months of Premium</h1>
         <p class="">Enjoy ad-free music listening, offline playback, and more. Cancel anytime.</p>
-        <div class="buttons">
+        <div class="buttons mt-[40px] mr-[10px]">
             <Button 
             element="a" 
             variant="outline" 
@@ -19,7 +19,7 @@
             <Button element="a" href="api/auth/login">Login</Button>
         </div>
     </div>
-    <div class="cover">
-        <img src={cover} alt="Svelte album cover" />
+    <div class="cover flex-[1] mt-[40px] md:mt-0 md:ml-5">
+        <img src={cover} alt="Svelte album cover" class="max-w-full" />
     </div>
 </div>
