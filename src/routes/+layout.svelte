6de251cc511de629e69a10bc7e826f74d3554cab
Cod3Andy <derangedjk75@gmail.com>
2023-08-06 03:54:50 +0300

    progress: Navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+    import Navigation from '$components/Navigation.svelte';
     import 'modern-normalize/modern-normalize.css';
     import "../app.css";
 	import type { LayoutData } from './$types';
@@ -8,8 +9,13 @@
     $: user = data.user;
 </script>
 
-<div id="main">
-    <div id="content">
+<div id="main" class="flex ">
+    {#if user}
+        <div id="sidebar">
+            <Navigation desktop={true} />
+        </div>
+    {/if}
+    <div id="content" class="flex-1">
         <main id="main-content" class="pt-[30px] pb-[60px] px-[15px] md:px-[30px]">
             <slot />
         </main>
