67f43e3406d6408a30bc42b80ec1c21492f1144b
Cod3Andy <derangedjk75@gmail.com>
2023-08-11 23:21:18 +0300

    add: working player & playlist styling

---
+++ src/lib/components/Player.svelte
@@ -0,0 +1,58 @@
+<script lang="ts" context="module">
+    let current: HTMLAudioElement;
+</script>
+<script lang="ts">
+	import { Pause, Play } from "lucide-svelte";
+    import { createEventDispatcher } from "svelte";
+
+    type Track = SpotifyApi.TrackObjectFull | SpotifyApi.TrackObjectSimplified;
+    export let track: Track;
+
+    const dispatch = createEventDispatcher<{
+        play: { track: Track };
+        pause: { track: Track };
+
+    }>();
+
+    let audio: HTMLAudioElement;
+    let paused = true;
+
+    function onPlay() {
+        if(current && current !== audio) {
+            current.currentTime = 0;
+            current.pause();
+        }
+        current = audio;
+        dispatch('play', { track });
+    }
+    function onPause() {
+        dispatch('pause', { track });
+    }
+</script>
+
+<div class="player">
+    <audio
+        on:play={onPlay}
+        on:pause={onPause}
+        class="hidden" 
+        bind:this={audio} 
+        bind:paused 
+        controls 
+        src={track.preview_url} 
+        preload="none" 
+    />
+    <button class="w-3 h-3 p-0 bg-transparent border-none cursor-pointer" aria-label={paused ? `Play ${track.name}` : `Pause ${track.name}`} on:click={() => {
+        if(paused) {
+            audio.play();
+        } else {
+            audio.pause();
+        }
+        }}
+        >
+        {#if paused}
+        <Play color="var(--text-color)" class="fill-text-color w-3 h-3" focusable="false" aria-hidden />
+        {:else}
+        <Pause color="var(--text-color)" class="fill-text-color w-3 h-3" focusable="false" aria-hidden />
+        {/if}
+    </button>
+</div>
--- src/lib/components/TrackList.svelte
+++ src/lib/components/TrackList.svelte
@@ -1,27 +1,48 @@
 <script lang="ts">
-	import { msToTime } from "$helpers";
-	import { Clock8, ListPlus } from "lucide-svelte";
+	import { Player } from '$components';
+    import { msToTime } from '$helpers';
+	import { Clock8, ListPlus } from 'lucide-svelte';
+    import playingGif from '$assets/playing.gif';
+
+    let currentlyPlaying: string | null = null;
+    let isPaused: boolean = false;
 
     export let tracks: SpotifyApi.TrackObjectFull[] | SpotifyApi.TrackObjectSimplified[];
 </script>
 
 <div class="tracks">
-    <div class="row-header">
-        <div class="number-column">
-            <span class="number">#</span>
+    <div class="row-header flex items-center py-[7px] px-[5px] border-b-[1px] border-border p-[5px] mb-[15px]">
+        <div class="number-column flex justify-end w-[30px] mr-[15px]">
+            <span class="number text-light-gray text-[14px]">#</span>
         </div>
         <div class="info-column">
-            <span class="track-title">Title</span>
+            <span class="track-title text-light-gray text-[12px] uppercase">Title</span>
         </div>
         <div class="duration-column">
-            <Clock8 aria-hidden focusable="false" />
+            <Clock8 aria-hidden focusable="false" color="var(--light-gray)" class="w-[16px] h-[16px]" />
         </div>
-        <div class="action-column"></div>
+        <div class="actions-column"></div>
     </div>
     {#each tracks as track, index}
-        <div class="row">
-            <div class="number-column">
-                <span class="number">{index + 1}</span>
+        <div class:is-current={currentlyPlaying === track.id} class="row flex items-center py-[7px] px-[5px] rounded-[4px] hover:bg-white/5">
+            <div class="number-column flex justify-end w-[30px] mr-[15px]">
+                {#if currentlyPlaying === track.id && !isPaused}
+                    <img class="playing-gif w-3 " src={playingGif} alt="" />
+                {:else}
+                    <span class="number text-light-gray text-[14px]">{index + 1}</span>
+                {/if}
+                <div class="player hidden">
+                    <Player 
+                        {track} 
+                        on:play={(e) => {
+                            currentlyPlaying = e.detail.track.id;
+                            isPaused = false;
+                        }}
+                        on:pause={(e) => {
+                            isPaused = e.detail.track.id === currentlyPlaying;
+                        }}
+                    />
+                </div>
             </div>
             <div class="info-column">
                 <div class="track-title">
@@ -30,19 +51,53 @@
                         <span class="explicit">Explicit</span>
                     {/if}
                 </div>
-                <p class="artists">
+                <p class="artists text-light-gray text-[13px] mt-[7px] px-0 pb-0 leading-none">
                     {#each track.artists as artist, artistIndex}
-                        <a href="/artists/{artist.id}">{artist.name}</a>{#if artistIndex <
+                        <a href="/artists/{artist.id}" class="text-inherit no-underline" >{artist.name}</a>{#if artistIndex <
                         track.artists.length - 1}{', '}{/if}
                     {/each}
                 </p>
             </div>
             <div class="duration-column">
-                <span class="duration">{msToTime(track.duration_ms)}</span>
+                <span class="duration text-light-gray text-[14px]">{msToTime(track.duration_ms)}</span>
             </div>
-            <div class="action-column">
-                <ListPlus aria-hidden focusable="false" />
+            <div class="actions-column ">
+                <ListPlus aria-hidden focusable="false" color="var(--light-gray)" />
             </div>
         </div>
     {/each}
 </div>
+<style>
+    .info-column {
+        @apply flex-1;
+        &.track-title {
+            @apply flex items-center;
+            &h4 {
+                @apply m-0 text-[15px] font-normal leading-none;
+            }
+            &span.explicit {
+                @apply uppercase text-[8px] ml-[10px] border-solid border-[1px] py-[2px] px-[3px] rounded-[2px] leading-[10px] text-light-gray;
+            }
+        }
+    }
+    .actions-column {
+        @apply w-[30px] ml-[15px];
+    }
+    .is-current .info-column .track-title h4, .is-current .number-column span.number {
+        @apply text-accent-color;
+    }
+    .row {
+        &:not(.header) {
+            &:hover .number-column .player {
+                @apply block;
+            }
+            &:hover .number-column span.number {
+                @apply hidden;
+            }
+            &:hover .number-column .playing-gif {
+                @apply hidden;
+            }
+        } 
+    }
+    /* BAD CODE ^^^ HOW TO DO THE SAME IN TAILWIND??? */
+</style>
